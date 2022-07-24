4f9e718e2e2e69c0144401d9df7061747d5d42fe
Zeder <kien@modmo.io>
2022-07-25 01:17:14 +0700

    add README

---
+++ README.md
@@ -0,0 +1,15 @@
+## Flash Management (Max 256 page - every page 128 byte)
+# Page 0 - 9 (0x0000 - 0x0480)
+	Page 0 (0x0000 - 0x007F): 12 byte information
+	Page 1 (0x0080 - 0x00FF) 
+	Page 2 (0x0100 - 0x017F)
+	Page 3 (0x0180 - 0x01FF)
+# Page 10 - 19
+    Page 0: 12 byte information
+	P11-19: epc storage
+# Page 20 - 29
+    Page 0: 12 byte information
+	P21-29: epc storage
+# Page 30 - 39
+    Page 0: 12 byte information
+	P31-39: epc storage
--- src/esp_uhf.ino
+++ src/esp_uhf.ino
@@ -2,8 +2,14 @@
 #include <Arduino.h>
 #include "RF_Commands.h"
 #include "HardwareSerial.h"
-#include <ios>
-#include <algorithm>    // std::equal
+#include <Wire.h>
+#include <AT24Cxx.h>
+
+#define i2c_address 0x50
+/** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
+int address = 32767;
+
+AT24Cxx eep(i2c_address, 32);
 HardwareSerial SerialRF(2);
 RFC_Class rfc(&SerialRF);
 
@@ -17,6 +23,37 @@ uint8_t epc_list[4][12] = {
 
 
 uint8_t test_source[12] = {0xE2,0x80,0x68, 0x94, 0x0,0x0, 0x50, 0x1A, 0x4D, 0x13, 0x6A, 0x18};
+
+void test_eeprom(){
+    int val = analogRead(0) / 4;
+	Serial.println(val);
+
+	eep.write(address, 'c');
+	/***
+	Advance to the next address, when at the end restart at the beginning.
+	***/
+	address = address + 1;
+	if (address == eep.length())
+	{
+        Serial.println("reset address");
+		address = 32767;
+	}
+
+    // read a byte from the current address of the EEPROM
+    byte value = eep.read(address);
+
+    Serial.print(address);
+    Serial.print("\t");
+    Serial.print(value, DEC);
+    Serial.println();
+
+    address = address + 1;
+    if (address == eep.length()) {
+        Serial.println("reset address");
+        address = 0;
+    }
+
+}
 bool find_in_epc_list(uint8_t input[]){
     for(int i = 0; i < 4 ; i++){
         if(compare(input, epc_list[i])){
@@ -38,20 +75,23 @@ bool compare(uint8_t input[], uint8_t source[]){
     }
     return count == 12;
 }
+
+void printContents();
 void setup()
 {
-    // pinMode(RFIDEN, OUTPUT);
-    // digitalWrite(RFIDEN, 0);
-    // delay(100);
-    // digitalWrite(RFIDEN, 1);
-    // delay(500);
-    //SerialRF.pins(15,13);
+    
     SerialRF.begin(115200, SERIAL_8N1,16,17);
     Serial.begin(115200);
     rfc.begin();
-
-    find_in_epc_list(test_source);
-     
+    Wire.begin();
+    //find_in_epc_list(test_source);
+    Serial.println ( "14CORE | 24C256 EEPROM TEST CODE"); // send a message to the serial monitor 	
+    Serial.println ( "--------------------------------");
+    Serial.println ( "START... ");
+    Wire.begin();
+    //
+    test_eeprom();
+    printContents();
 }
 void uhf_read_user_data(){
     
@@ -77,10 +117,30 @@ void flow(){
        Serial.printf("Error Code: %X\n", rfc.error.ErrorCode());
     }
 }
+void test_flash(){
+    while (Serial.available()) {
+    int i, m;
+    String data = Serial.readStringUntil('\n');
+ 
+    for (i = 0, m = data.length(); i < m; ++i) {
+      eep.write(i, data[i]);
+	  
+	  Serial.println("14CORE | AT24C256 WRITE CODE TEST");
+	  Serial.println("---------------------------------");
+      Serial.println("Writing  : ");
+      Serial.print(data[i]);
+      Serial.println(" IN : ");
+      Serial.println(i);
+    }
+    eep.write(i, 0xFF); //Start write 
+    printContents();
+  }
+  delay(1);
+}
 void loop()
 {
 
-    delay(200);
+    //delay(500);
     
     // while (!SerialRF.available()){
 
@@ -88,8 +148,27 @@ void loop()
     //     continue;
     // }
     
+    //
+    //flow();
 
-    flow();
 
-    
+    //Test Flash
+    test_flash();
 }
+
+
+ 
+void printContents() {
+int eeaddress = 32767;
+byte value;
+ 
+  while (true) {
+    value = eep.read(eeaddress++);
+    if ((value == 0xFF) || (eeaddress == 32768)) {
+      Serial.println();
+      break;
+    }
+    Serial.print((char)value);
+  }
+}
+ 
