bee8378ca954de1276ea764bb6e55cd71d10ca8e
Zeder <kien.ngo@fuelcloud.com>
2023-08-25 22:28:56 +0700

    - change wifi from client to ap mode

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -664,34 +664,191 @@ bool initWiFi() {
   return true;
 }
 
+// void WebSetup(){
+
+
+//     ssid = readFile(SPIFFS, ssidPath);
+//     pass = readFile(SPIFFS, passPath);
+//     ip = readFile(SPIFFS, ipPath);
+//     gateway = readFile (SPIFFS, gatewayPath);
+//     Serial.println(ssid);
+//     Serial.println(pass);
+//     Serial.println(ip);
+//     Serial.println(gateway);
+
+//     if(initWiFi()) {
+//         // Route for root / web page
+//         server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+//         request->send(SPIFFS, "/index.html", String(), false, processor);
+//         });
+//         //Route to load style.css file
+//         server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request){
+//         request->send(SPIFFS, "/styles.css", "text/css");
+//         });
+        
+//         initWebSocket();
+
+//         server.begin();
+//     }
+//     else {
+//       // Connect to Wi-Fi network with SSID and password
+//       Serial.println("Setting AP (Access Point)");
+//       // NULL sets an open Access Point
+//       WiFi.softAP("WIFI-MANAGER", "123456789");
+
+//       IPAddress IP = WiFi.softAPIP();
+//       Serial.print("AP IP address: ");
+//       Serial.println(IP); 
+
+//       // Web Server Root URL
+//       server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+//         request->send(SPIFFS, "/wifimanager.html", "text/html");
+//       });
+
+//       server.serveStatic("/", SPIFFS, "/");
+      
+//       server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+//         int params = request->params();
+//         for(int i=0;i<params;i++){
+//           AsyncWebParameter* p = request->getParam(i);
+//           if(p->isPost()){
+//             // HTTP POST ssid value
+//             if (p->name() == PARAM_INPUT_1) {
+//               ssid = p->value().c_str();
+//               Serial.print("SSID set to: ");
+//               Serial.println(ssid);
+//               // Write file to save value
+//               writeFile(SPIFFS, ssidPath, ssid.c_str());
+//             }
+//             // HTTP POST pass value
+//             if (p->name() == PARAM_INPUT_2) {
+//               pass = p->value().c_str();
+//               Serial.print("Password set to: ");
+//               Serial.println(pass);
+//               // Write file to save value
+//               writeFile(SPIFFS, passPath, pass.c_str());
+//             }
+//             // HTTP POST ip value
+//             if (p->name() == PARAM_INPUT_3) {
+//               ip = p->value().c_str();
+//               Serial.print("IP Address set to: ");
+//               Serial.println(ip);
+//               // Write file to save value
+//               writeFile(SPIFFS, ipPath, ip.c_str());
+//             }
+//             // HTTP POST gateway value
+//             if (p->name() == PARAM_INPUT_4) {
+//               gateway = p->value().c_str();
+//               Serial.print("Gateway set to: ");
+//               Serial.println(gateway);
+//               // Write file to save value
+//               writeFile(SPIFFS, gatewayPath, gateway.c_str());
+//             }
+//             //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+//           }
+//         }
+//         request->send(200, "text/plain", "Cai dat xong. Vui long truy cap trang web chinh tai dia chi ip sau: " + ip);
+//         delay(3000);
+//         ESP.restart();
+//       });
+//       server.begin();
+//     }
+
+// }
+
+
 void WebSetup(){
 
 
-    ssid = readFile(SPIFFS, ssidPath);
-    pass = readFile(SPIFFS, passPath);
-    ip = readFile(SPIFFS, ipPath);
-    gateway = readFile (SPIFFS, gatewayPath);
-    Serial.println(ssid);
-    Serial.println(pass);
-    Serial.println(ip);
-    Serial.println(gateway);
+    // ssid = readFile(SPIFFS, ssidPath);
+    // pass = readFile(SPIFFS, passPath);
+    // ip = readFile(SPIFFS, ipPath);
+    // gateway = readFile (SPIFFS, gatewayPath);
+    // Serial.println(ssid);
+    // Serial.println(pass);
+    // Serial.println(ip);
+    // Serial.println(gateway);
 
-    if(initWiFi()) {
-        // Route for root / web page
-        server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/index.html", String(), false, processor);
-        });
-        //Route to load style.css file
-        server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/styles.css", "text/css");
-        });
+    // if(initWiFi()) {
+    //     // Route for root / web page
+    //     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    //     request->send(SPIFFS, "/index.html", String(), false, processor);
+    //     });
+    //     //Route to load style.css file
+    //     server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    //     request->send(SPIFFS, "/styles.css", "text/css");
+    //     });
         
-        initWebSocket();
+    //     initWebSocket();
 
-        server.begin();
-    }
-    else {
-      // Connect to Wi-Fi network with SSID and password
+    //     server.begin();
+    // }
+    // else {
+    //   // Connect to Wi-Fi network with SSID and password
+    //   Serial.println("Setting AP (Access Point)");
+    //   // NULL sets an open Access Point
+    //   WiFi.softAP("WIFI-MANAGER", "123456789");
+
+    //   IPAddress IP = WiFi.softAPIP();
+    //   Serial.print("AP IP address: ");
+    //   Serial.println(IP); 
+
+    //   // Web Server Root URL
+    //   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    //     request->send(SPIFFS, "/wifimanager.html", "text/html");
+    //   });
+
+    //   server.serveStatic("/", SPIFFS, "/");
+      
+    //   server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    //     int params = request->params();
+    //     for(int i=0;i<params;i++){
+    //       AsyncWebParameter* p = request->getParam(i);
+    //       if(p->isPost()){
+    //         // HTTP POST ssid value
+    //         if (p->name() == PARAM_INPUT_1) {
+    //           ssid = p->value().c_str();
+    //           Serial.print("SSID set to: ");
+    //           Serial.println(ssid);
+    //           // Write file to save value
+    //           writeFile(SPIFFS, ssidPath, ssid.c_str());
+    //         }
+    //         // HTTP POST pass value
+    //         if (p->name() == PARAM_INPUT_2) {
+    //           pass = p->value().c_str();
+    //           Serial.print("Password set to: ");
+    //           Serial.println(pass);
+    //           // Write file to save value
+    //           writeFile(SPIFFS, passPath, pass.c_str());
+    //         }
+    //         // HTTP POST ip value
+    //         if (p->name() == PARAM_INPUT_3) {
+    //           ip = p->value().c_str();
+    //           Serial.print("IP Address set to: ");
+    //           Serial.println(ip);
+    //           // Write file to save value
+    //           writeFile(SPIFFS, ipPath, ip.c_str());
+    //         }
+    //         // HTTP POST gateway value
+    //         if (p->name() == PARAM_INPUT_4) {
+    //           gateway = p->value().c_str();
+    //           Serial.print("Gateway set to: ");
+    //           Serial.println(gateway);
+    //           // Write file to save value
+    //           writeFile(SPIFFS, gatewayPath, gateway.c_str());
+    //         }
+    //         //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+    //       }
+    //     }
+    //     request->send(200, "text/plain", "Cai dat xong. Vui long truy cap trang web chinh tai dia chi ip sau: " + ip);
+    //     // delay(3000);
+    //     // ESP.restart();
+    //   });
+    //   server.begin();
+    // }
+
+
+    // Connect to Wi-Fi network with SSID and password
       Serial.println("Setting AP (Access Point)");
       // NULL sets an open Access Point
       WiFi.softAP("WIFI-MANAGER", "123456789");
@@ -700,58 +857,18 @@ void WebSetup(){
       Serial.print("AP IP address: ");
       Serial.println(IP); 
 
-      // Web Server Root URL
-      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/wifimanager.html", "text/html");
-      });
-
       server.serveStatic("/", SPIFFS, "/");
       
-      server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
-        int params = request->params();
-        for(int i=0;i<params;i++){
-          AsyncWebParameter* p = request->getParam(i);
-          if(p->isPost()){
-            // HTTP POST ssid value
-            if (p->name() == PARAM_INPUT_1) {
-              ssid = p->value().c_str();
-              Serial.print("SSID set to: ");
-              Serial.println(ssid);
-              // Write file to save value
-              writeFile(SPIFFS, ssidPath, ssid.c_str());
-            }
-            // HTTP POST pass value
-            if (p->name() == PARAM_INPUT_2) {
-              pass = p->value().c_str();
-              Serial.print("Password set to: ");
-              Serial.println(pass);
-              // Write file to save value
-              writeFile(SPIFFS, passPath, pass.c_str());
-            }
-            // HTTP POST ip value
-            if (p->name() == PARAM_INPUT_3) {
-              ip = p->value().c_str();
-              Serial.print("IP Address set to: ");
-              Serial.println(ip);
-              // Write file to save value
-              writeFile(SPIFFS, ipPath, ip.c_str());
-            }
-            // HTTP POST gateway value
-            if (p->name() == PARAM_INPUT_4) {
-              gateway = p->value().c_str();
-              Serial.print("Gateway set to: ");
-              Serial.println(gateway);
-              // Write file to save value
-              writeFile(SPIFFS, gatewayPath, gateway.c_str());
-            }
-            //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
-          }
-        }
-        request->send(200, "text/plain", "Cai dat xong. Vui long truy cap trang web chinh tai dia chi ip sau: " + ip);
-        delay(3000);
-        ESP.restart();
-      });
+      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(SPIFFS, "/index.html", String(), false, processor);
+        });
+        //Route to load style.css file
+        server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(SPIFFS, "/styles.css", "text/css");
+        });
+        
+      initWebSocket();
+
       server.begin();
-    }
 
 }
